c60aec67bb2f2c7d04afb2c4a6408d56337ecc52
samsung <ssossoking@naver.com>
2020-11-21 19:36:50 +0900

    노트북 커밋 테스트

--- src/test/java/com/ssosso/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/ssosso/book/springboot/web/HelloControllerTest.java
@@ -25,7 +25,7 @@ public class HelloControllerTest {
 
     @Test
     public void hello가_리턴된다() throws Exception{
-        String hello = "hello";
+        String hello = "hello!!";
 
         mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
     }
